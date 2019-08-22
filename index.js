00aeaa5da8960ec565d31f1204e43b65f1ae9931
LaRaye Johnson <lnj2105@gmail.com>
2019-08-22 03:55:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function scuberGreetingForFeet(someFeet){
   let riderMessage;
   if (someFeet <= 400) {
     riderMessage = 'This one is on me!'
+  } else if (someFeet >= 2000) {
+    riderMessage = 'I will gladly take your thirty bucks.'
   }
   
   return riderMessage
